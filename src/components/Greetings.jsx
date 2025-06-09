462a2d4a7fb3f4cc6c199bf215dae9e2e4ab151c
mohamed <lanjes991@gmail.com>
2025-06-09 08:36:03 +0300

    first commit

---
+++ src/components/Greetings.jsx
@@ -0,0 +1,6 @@
+function Greeting(props) {
+  return <h1>Hello, {props.name} {props.surname} {props.lastname}!</h1>;
+  
+}
+
+export default Greeting;
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>this is my first react tutorial</div>
+  )
+}
+
+export default Header
